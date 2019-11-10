07031b13b3768f9f62e3b8399bf5eaad41403f85
Michael Foster <jmafoster1@gmail.com>
2019-11-10 10:26:04 +0000

    Changed SingleOutputFitness to use a for loop rather than an iterator

--- mint-inference/src/main/java/mint/inference/gp/fitness/SingleOutputFitness.java
+++ mint-inference/src/main/java/mint/inference/gp/fitness/SingleOutputFitness.java
@@ -1,120 +1,101 @@
 package mint.inference.gp.fitness;
 
-import org.apache.log4j.Logger;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.Map.Entry;
+
 import mint.inference.gp.CallableNodeExecutor;
 import mint.inference.gp.tree.Node;
 import mint.tracedata.types.VariableAssignment;
-
-import java.util.ArrayList;
-import java.util.Iterator;
-import java.util.List;
-import java.util.Map;
 
 /**
  * Created by neilwalkinshaw on 05/03/15.
  */
 public abstract class SingleOutputFitness<T> extends Fitness {
 
+	final Map<List<VariableAssignment<?>>, VariableAssignment<?>> evalSet;
 
+	protected final int maxDepth;
+	protected Node<VariableAssignment<T>> individual;
+	List<Double> distances;
 
-    final Map<List<VariableAssignment<?>>,VariableAssignment<?>> evalSet;
+	public SingleOutputFitness(Map<List<VariableAssignment<?>>, VariableAssignment<?>> evals,
+			Node<VariableAssignment<T>> individual, int maxDepth) {
+		this.evalSet = evals;
+		this.individual = individual;
+		this.maxDepth = maxDepth;
+		this.distances = new ArrayList<Double>();
+	}
 
-    private final static Logger LOGGER = Logger.getLogger(SingleOutputFitness.class.getName());
+	public Node<?> getIndividual() {
+		return individual;
+	}
 
-    protected final int maxDepth;
-    //protected boolean penalize = false;
-    protected Node<VariableAssignment<T>> individual;
-    List<Double> distances;
+	public List<Double> getDistances() {
+		return distances;
+	}
 
-    public SingleOutputFitness(Map<List<VariableAssignment<?>>, VariableAssignment<?>> evals, Node<VariableAssignment<T>> individual, int maxDepth){
-        this.evalSet = evals;
-        this.individual = individual;
-        this.maxDepth = maxDepth;
-        //removeDoubleEvals();
-        this.distances = new ArrayList<Double>();
-    }
+	@Override
+	public Double call() throws InterruptedException {
+		distances.clear();
+		double penaltyFactor = 0;
+		for (Entry<List<VariableAssignment<?>>, VariableAssignment<?>> current : evalSet.entrySet()) {
+			boolean penalize = false;
+			if (Thread.interrupted())
+				throw new InterruptedException();
+			double distance = 0D;
+			T actual = null;
+			try {
+				CallableNodeExecutor<T> executor = new CallableNodeExecutor<T>(individual, current.getKey());
 
-    public Node<?> getIndividual(){
-        return individual;
-    }
+				try {
+					actual = executor.call();
+				} catch (Exception e) { // GP candidate has crashed.
+					e.printStackTrace();
+					penaltyFactor = 100;
+				}
+				if (actual == null)
+					penaltyFactor = 100;
+				if (!penalize) {
+					distance = (distance(actual, current.getValue().getValue()));
+					distances.add(distance);
+				}
+			} catch (InvalidDistanceException e) {
+				penaltyFactor = 100;
+			}
+			if (individual.subTreeMaxdepth() > maxDepth)
+				penaltyFactor = Math.abs((individual.subTreeMaxdepth() - maxDepth) * 2);
 
-    public List<Double> getDistances(){
-        return distances;
-    }
+			distances.add(distance);
+		}
+		double distance = calculateFitness(distances);
+		return distance + penaltyFactor;
+	}
 
+	protected Double calculateFitness(List<Double> distances) {
+		return rmsd(distances);
+	}
 
-    public Double call() throws InterruptedException {
-        Iterator<List<VariableAssignment<?>>> inputIt = evalSet.keySet().iterator();
-        distances.clear();
-        double penaltyFactor = 0;
-        while(inputIt.hasNext()){
-            boolean penalize = false;
-            if(Thread.interrupted())
-                throw new InterruptedException();
-            List<VariableAssignment<?>> current = inputIt.next();
-            VariableAssignment<?> expectedVar = evalSet.get(current);
-            T expected = (T) expectedVar.getValue();
-            double distance = 0D;
-            T actual = null;
-            try {
-                CallableNodeExecutor<T> executor = new CallableNodeExecutor<T>(individual,current);
+	protected abstract double distance(T actual, Object expected) throws InvalidDistanceException;
 
-                try{
-                    actual = executor.call();
-                }
-                catch(Exception e){  //GP candidate has crashed.
-                    e.printStackTrace();
-                    penaltyFactor=100;
-                }
-                if(actual == null)
-                    penaltyFactor=100;
-                if(!penalize) {
-                    distance = (distance(actual, expected));
-                    distances.add(distance);
-                }
-            }
-            catch(InvalidDistanceException e){
-                penaltyFactor=100;
-            }
-            if(individual.subTreeMaxdepth() > maxDepth)
-                penaltyFactor = Math.abs((individual.subTreeMaxdepth() - maxDepth)*2);
-                //distance+=distance/2;
+	@Override
+	public boolean equals(Object o) {
+		if (this == o)
+			return true;
+		if (!(o instanceof SingleOutputFitness))
+			return false;
 
-            distances.add(distance);
-        }
-        //if(penalize) {
-            //service.shutdown();
-          //  return 3 * distance;//getPenalty();
-        //}
-        ///else {
-            //service.shutdown();
-            double distance = calculateFitness(distances);
-            return distance + penaltyFactor;
-       // }
+		SingleOutputFitness singleOutputFitness = (SingleOutputFitness) o;
 
-    }
+		if (!individual.equals(singleOutputFitness.individual))
+			return false;
 
+		return true;
+	}
 
-    protected Double calculateFitness(List<Double> distances) {
-        return rmsd(distances);
-    }
-
-    protected abstract double distance(T actual, Object expected) throws InvalidDistanceException;
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (!(o instanceof SingleOutputFitness)) return false;
-
-        SingleOutputFitness singleOutputFitness = (SingleOutputFitness) o;
-
-        if (!individual.equals(singleOutputFitness.individual)) return false;
-
-        return true;
-    }
-
-    @Override
-    public int hashCode() {
-        return individual.hashCode();
-    }
+	@Override
+	public int hashCode() {
+		return individual.hashCode();
+	}
 }
